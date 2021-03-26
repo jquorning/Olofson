dd87371257a9498a1e993a3c3dd998925b183f05
Jesper Quorning <jesper.quorning@gmail.com>
2021-03-26 16:52:55 +0100

    Remove uncommented command line option variables.

--- parallax_4/parallax_4.adb
+++ parallax_4/parallax_4.adb
@@ -202,16 +202,6 @@ package body Parallax_4 is
                                             then SDL.Video.Windows.Full_Screen
                                             else 0);
 
-      --  subtype Verbosity is Integer range 0 .. 3;
-      --  Bpp           : Integer   := 0;
-      --  Flags         : Integer   := 0;
-      --  Verbose       : Verbosity := 0;
-      --  Use_Planets   : Boolean   := True;
-      --  Num_Of_Layers : Integer   := 7;
-      --  Bounce_Around : Boolean   := False;
-      --  Wrap          : Boolean   := False;
-      --  Alpha         : Integer   := 0;
-
       Layers : array (0 .. Num_Of_Layers - 1) of aliased Layer_Type;
 
       Total_Blits      : Natural;
